e93c75a9778100da6880ce6d1d7e13aa72c14401
Screfox <142076193+ScreamerFox@users.noreply.github.com>
2025-02-20 23:44:55 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,10 @@ from app.models import Base
 app = FastAPI()
 app.include_router(endpoints_router)
 
-
+@app.on_event("startup")
+async def on_startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
 
 if __name__ == "__main__":
